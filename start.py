025d950b1f58a1aa0537c8b2d20e4c70d02f59b5
sanjana <icodesanjana@gmail.com>
2026-07-21 22:50:00 +0530

    initial commit

---
+++ start.py
@@ -0,0 +1,48 @@
+# import os
+# import flask
+
+# import pyjokes
+# print("hello world")
+# joke = pyjokes.get_joke()
+# print(joke)
+
+# directory_path = '/'
+# content = os.listdir(directory_path)
+# for item in content:
+#     print(item)
+a = 12
+b = 23
+print(a+b)
+print("remainder a is divided by b" , a % b)
+print(a%b)
+
+a = ("enter the value of a")
+print(type(a))
+
+#inputs
+# a = int(input("enter the number 1:"))
+# print("number a is : ", a)
+# print("number b is : ", b)
+# print("sum", a + b)
+
+# b = int(input("enter the number 2:"))
+#practise set
+
+# a = int(input("enter the number 1:"))
+# b = int(input("enter the number 2:"))
+# print("square of thes numbers",  a**2)
+
+name = "sanjana"
+name1 = name[-7:-3]
+names = name[3:7]
+name2 = len(name)
+print(name1)
+print(name2)
+print(names)
+
+nameo = "halemani"
+print(nameo.endswith("ni"))
+print(nameo.startswith("ha"))
+a = "hi i am boy" 
+a.replace("boy", "girl")
+print(a)
