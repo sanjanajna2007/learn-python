40629fcef62182fa971a89ddaa6657f96fdda3e2
sanjana <icodesanjana@gmail.com>
2026-07-27 10:18:07 +0530

    added hello files

---
+++ sets.py
@@ -0,0 +1,39 @@
+# s = {1,3,5,45,45,5}
+
+# e = set()
+# print(s)
+# s1 = {11,25,56}
+# s2 = {4,78,26,56}
+# print(s1.union(s2))
+# print(s1.intersection(s2))
+# print(s1.issubset(s2))
+# print(s1.issuperset(s2))
+
+# dict = {
+#     "madad" : "help",
+#     "shukriya" : "thankyou",
+#     "kaam" : "work",
+#     "paani" : "water"
+# }
+# word=input("enter thr word you want to create :")
+# print(dict[word])
+
+# s = set()
+# n = input("enter a number")
+# s.add(int(n))
+# n = input("enter a number")
+# s.add(int(n))
+# n = input("enter a number")
+# s.add(int(n))
+# n = input("enter a number")
+# s.add(int(n))
+# n = input("enter a number")
+# s.add(int(n))
+# print(s)
+
+s= set()
+s.add(20)
+s.add(20.0)
+s.add('20')
+
+print(s)
