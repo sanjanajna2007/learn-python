c23591c0adc6da47aa4177be86c09d3f5784cb7a
sanjana <icodesanjana@gmail.com>
2026-07-28 16:00:53 +0530

    added hema files

--- hema.py
+++ hema.py
@@ -1,12 +1,43 @@
-marks1 = int(input("enter a marks 1 "))
-marks2 = int(input("enter a marks 2 "))
-marks3 = int(input("enter a marks 3 "))
+# marks1 = int(input("enter a marks 1 "))
+# marks2 = int(input("enter a marks 2 "))
+# marks3 = int(input("enter a marks 3 "))
 
-total_percentage = (marks1+ marks2 + marks3)/300*100
-print(total_percentage)
+# total_percentage = (marks1+ marks2 + marks3)/300*100
+# print(total_percentage)
 
-if(total_percentage>=40):
-    print("you are pass")
+# if(total_percentage>=40):
+#     print("you are pass")
+
+# else:
+#     print("you are failed");
+
+# p1 = "make broo"
+# p2 = "buy now"
+# p3 = "whyy this is broo"
+
+# message = input("enter your comment")
+
+# if((p1 in message) or (p2 in message) or (p3 in message)):
+#     print("this message is spam")
+
+# else:
+#     print("tnis message is not spam")
+
+
+# username = input("enter user name ")
+
+# if(len(username)<10):
+#     print("username must contains less than 10 characters")
+
+# else:
+#     print("username contains more than 10 characters")
+
+s = ["sanzz", "max", "google"]
+
+name = input("enter your name ")
+
+if(name in s):
+    print("your name in the list")
 
 else:
-    print("you are failed")
+    print("not in the list")
