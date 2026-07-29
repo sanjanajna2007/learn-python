f54f468dacf53db2a61d76d5e52a827541e52a4b
sanjana <icodesanjana@gmail.com>
2026-07-29 20:18:09 +0530

    pushing loops files

---
+++ loops.py
@@ -0,0 +1,13 @@
+# post = input("enter the post ")
+
+# if("harry".lower() in post.lower()):
+#     print("talking abt harry")
+
+# else:
+#     print("talking about others")
+
+            #   loops
+
+
+for i in range (1):
+    print(i)
