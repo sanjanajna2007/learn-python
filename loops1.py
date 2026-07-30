ee452a90201900882e3264e71f21a66094088658
sanjana <icodesanjana@gmail.com>
2026-07-30 19:41:14 +0530

    added loops files

---
+++ loops1.py
@@ -0,0 +1,35 @@
+# post = input("enter the post ")
+
+# if("harry".lower() in post.lower()):
+#     print("talking abt harry")
+
+# else:
+#     print("talking about others")
+
+            #   loops
+
+
+# for i in range (1):
+#     print(i)
+
+# while(i<=5):
+#     print(i)
+#     i=i+1 
+
+# l = [1, "sanjuu", "true", "this", 2, 12]
+# i=0
+# while(i<len(l)):
+#     print(l[i])
+#     i+=1
+
+
+# for i in range(5):
+#     print(i)
+
+s = [1,2,3,4]
+for i in s:
+    print(i)
+
+s1 = "sanju"
+for i in s1:
+    print(i)
