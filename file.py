1c1f789e4176df46aa913b52dc671727077ca8c8
sanjana <icodesanjana@gmail.com>
2026-08-20 15:45:01 +0530

    updated code

--- file.py
+++ file.py
@@ -3,7 +3,25 @@
 # print(data)
 # f.close
 
-st = "heyyy amazing to meet ypu are u kidding"
-f = open("myfile.txt", "w")
+st = "heyyy amazing to meet ypu are u kidding soo sweet of you"
+f = open("myfile.txt", "a")
 f.write(st)
 f.close
+
+f = open("myfile.txt")
+print(f.read())
+f.close()
+
+with open("myfile.txt") as f:
+    print(f.read())
+
+    # no need to close the file
+
+    f = open("myfile.txt")
+    content = f.read()
+    if("kidding" in content):
+        print("yes this is correct file")
+
+    else:
+        print("this is not correct file")
+    f.close()
