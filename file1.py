948ff7735c657463c235343def7f8eeac684d3a0
sanjana <icodesanjana@gmail.com>
2026-08-23 13:13:10 +0530

    adding copy files

---
+++ file1.py
@@ -0,0 +1,11 @@
+with open("file1.txt", "w") as f:
+    f.write("")
+
+# with open("myfile.txt", "r") as f:
+#     content2 = f.read()
+
+# if (content1 == content2):
+#     print("yes these files are identical")
+
+# else:
+#     print("these files are not identical")
