03aae4ebdf2af32d7bd49b49d02dad0fe9c397a1
sanjana <icodesanjana@gmail.com>
2026-08-23 12:57:58 +0530

    updated code

--- mul.py
+++ mul.py
@@ -1,11 +1,21 @@
-def multiplication(n):
-    table = ""
-    for i in range(1, 11):
-        table += f"{n} * {i} = {n*i}\n"
+# def multiplication(n):
+#     table = ""
+#     for i in range(1, 11):
+#         table += f"{n} * {i} = {n*i}\n"
 
-    with open(f"table.txt{n}", "w") as f:
-        f.write(table)
+#     with open(f"table.txt{n}", "w") as f:
+#         f.write(table)
 
-for i in range(2, 21):
+# for i in range(2, 21):
 
-    multiplication(i)
+#     multiplication(i)
+
+words = ["donkey", "lorem", "gunga"]
+
+with open("highscore.txt", "r") as f:
+    content = f.read()
+for word in words:
+    content = content.replace(word, "#" * len(word))
+
+with open("highscore.txt", "w") as f:
+    f.write(content)
