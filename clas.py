c3a4fe8956f09c239c1cc370997c530c42b12dcf
sanjana <icodesanjana@gmail.com>
2026-08-24 14:23:57 +0530

    adding class files

---
+++ clas.py
@@ -0,0 +1,18 @@
+# class car:
+#     def __init__(self,rate):
+#         print("constructor is calling")
+#         self.rate = rate
+#     def walk(self):
+
+# c = car("doller")
+
+# c.walk()
+
+class employee:
+    language = "py"
+    sallary = 12000
+
+harry = employee()
+print(harry.language,harry.sallary  )
+
+
