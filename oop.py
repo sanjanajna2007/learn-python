08d14552c1fe676ccbeead59aa211b3d7c4c3b3b
sanjana <icodesanjana@gmail.com>
2026-08-26 19:55:58 +0530

    adding imp concepts

--- oop.py
+++ oop.py
@@ -8,3 +8,32 @@ class ATM:
 cbi = ATM(1000)
 
 print(cbi.check_balance)
+
+
+    #   creating te database
+
+class database:
+    def __init__(self):
+        self.storage = {} #public
+        self._storage = {} #protected
+        self.__storage = {} #private
+
+    def write(self, key, value):
+        self.__storage[key] = value
+
+    def read(self, key):
+        if key in self.__storage:
+           
+            return self.__storage[key]
+
+        else:
+            print("db is not available")
+         
+
+db = database()
+db.read( "subscribers")
+db.write("subscribers", "10k")
+print(db.storage)
+
+db.read("name")
+
