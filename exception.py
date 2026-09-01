7d05643b67c2adc1bb6b8ee1673af1b4a688d636
sanjana <icodesanjana@gmail.com>
2026-09-01 15:40:00 +0530

    updating exception files

--- exception.py
+++ exception.py
@@ -1,24 +1,24 @@
-# # a = 10
-# # b = 0
-# # print(a/b)
+a = 10
+b = 0
+print(a/b)
 
-# # a = int(input("a "))
-# # b = int(input("b "))
+a = int(input("a "))
+b = int(input("b "))
 
-# # print(a/b)
+print(a/b)
 
-# try:
-#     print(a/b)
-# except Exception as e :
-#     print(f"error: {e}")
-#     b = int(input("b: "))
-#     print(a/b)
+try:
+    print(a/b)
+except Exception as e :
+    print(f"error: {e}")
+    b = int(input("b: "))
+    print(a/b)
 
-# else:
-#     print("no error")
-# finally:
-#     print("i dont care")
-#     print("programm is ended")
+else:
+    print("no error")
+finally:
+    print("i dont care")
+    print("programm is ended")
 
 try:
     num = int(input("enter a numbrer"))
@@ -28,3 +28,11 @@ except ZeroDivisionError:
     print("it is cannot divide by zero")
 except ValueError:
     print("please enter a valid number")
+
+try:
+    boy = input("who do you want to love")
+    if boy.lower() != "chandan":
+        raise Exception("you can love only him not others")
+
+except Exception as e:
+    print(f"error: {e}")
