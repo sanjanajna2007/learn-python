ae82e047c4f74680d8c94572ac74d9ed6cd46be4
sanjana <icodesanjana@gmail.com>
2026-09-01 15:31:27 +0530

    adding exception files

---
+++ exception.py
@@ -0,0 +1,30 @@
+# # a = 10
+# # b = 0
+# # print(a/b)
+
+# # a = int(input("a "))
+# # b = int(input("b "))
+
+# # print(a/b)
+
+# try:
+#     print(a/b)
+# except Exception as e :
+#     print(f"error: {e}")
+#     b = int(input("b: "))
+#     print(a/b)
+
+# else:
+#     print("no error")
+# finally:
+#     print("i dont care")
+#     print("programm is ended")
+
+try:
+    num = int(input("enter a numbrer"))
+    result = 10/num
+
+except ZeroDivisionError:
+    print("it is cannot divide by zero")
+except ValueError:
+    print("please enter a valid number")
