a4900b9fb9d174ef843ea7faf0c53d6b5cf62fad
sanjana <icodesanjana@gmail.com>
2026-09-05 00:14:32 +0530

    adding QR code

--- module.py
+++ module.py
@@ -2,5 +2,20 @@
 
 # math.sqrt(25)
 
-import wikipedia
-print(wikipedia.summary("sanjana"))
+# import wikipedia
+# print(wikipedia.summary("sanjana"))
+
+# from currency_converter import CurrencyConverter
+
+# c = CurrencyConverter()
+# amt = float(input("enter a amount"))
+# new_amt = c.convert(1, "USD", "INR", date=(2013,3,21))
+# print(f"amount in INR:{new_amt} ")
+
+
+import qrcode
+image = qrcode.make("https://www.linkedin.com/in/sanjana-j-halemani-86394833")
+
+image.save("eik_qr.png")
+
+print("qr code generated successfully")
